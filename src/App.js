9000c7e4be8debfbb33e24839f542cee89b11044
seeys <es2008e@naver.com>
2022-06-21 00:19:14 +0900

    init and input update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import styled from "@emotion/styled";
 import Header from "./components/Header";
 import InputImageBox from "./components/InputImageBox";
-import ResultImageBox from "./components/ResultImageBox";
 
 function App() {
   return (
     <div style={{ margin: "10px" }}>
       <Header>나의 닮은 꼴은?</Header>
       <InputImageBox />
-      <ResultImageBox>결과</ResultImageBox>
     </div>
   );
 }
--- src/components/InputImageBox.jsx
+++ src/components/InputImageBox.jsx
@@ -1,8 +1,5 @@
 import styled from "@emotion/styled";
-import axios from "axios";
 import React from "react";
-import { useRef, useState } from "react";
-import { BsPlusSquare } from "react-icons/bs";
 
 const BoxContainer = styled.div`
   border: #087f5b solid 5px;
@@ -14,9 +11,16 @@ const BoxContainer = styled.div`
   align-items: center;
   margin: 0 auto;
   margin-bottom: 10px;
+`;
 
-  .test:hover {
-    color: #96f2d7;
+const InsertBtn = styled.div`
+  border: #087f5b solid 5px;
+  border-radius: 30px;
+  font-size: 20px;
+  padding: 10px;
+
+  &:hover {
+    color: red;
   }
 `;
 
@@ -24,64 +28,29 @@ const PreviewBox = styled.div`
   text-align: center;
 `;
 
-const InputImageBox = () => {
-  const [file, setFile] = useState(null);
-  const [result, setResult] = useState("");
-  const imgInput = useRef("");
-
-  // API로 데이터를 받아오는 함수
-  const fetchData = async (form) => {
-    try {
-      // '/api/search'로 서버에 요청
-      const { data } = await axios.get("/face", {
-        formData: form,
-      });
-
-      setResult(data);
-    } catch (error) {
-      let message = "Unknown Error";
-      if (error instanceof Error) message = error.message;
-      console.log(message);
-    }
-  };
-
-  // 파일 변경 감지 핸들러
-  const onClickFile = (event) => {
-    event.preventDefault();
-    const img = event.target.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(img);
-    const form = new FormData();
-    form.append("image", img);
-    fetchData(form);
-    console.log(result);
-    return new Promise((res) => {
-      reader.onload = () => {
-        setFile(reader.result);
-        res();
-      };
-    });
+function InputImageBox() {
+  // 파일 보내기
+  const onClickFile = (e) => {
+    console.log("hi");
+    console.log(e.target.files[0]);
   };
 
   return (
     <>
       <BoxContainer>
         <label htmlFor="input_file">
-          <BsPlusSquare size="100" className="test" />
-          <input
-            type="file"
-            style={{ display: "none" }}
-            id="input_file"
-            onChange={onClickFile}
-            ref={imgInput}
-          />
+          <InsertBtn>insert</InsertBtn>
         </label>
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="input_file"
+          onChange={onClickFile}
+        />
       </BoxContainer>
-      <PreviewBox>
-        {file && <img src={file} alt="preview" width="200" height="200" />}
-      </PreviewBox>
+      <PreviewBox>미리보기</PreviewBox>
     </>
   );
-};
+}
 
 export default InputImageBox;
