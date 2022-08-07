e5cf473c6d73457511ee3a5b3efb69fa5d41b8ea
seeys <es2008e@naver.com>
2022-08-07 23:22:43 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://localhost:4000",
+      target: "http:/54.88.114.24:4000",
       changeOrigin: true,
     })
   );
